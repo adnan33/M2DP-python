7c6654fe32933c166d5e2f82cd09a5f0da38468f
adnan33 <44573741+adnan33@users.noreply.github.com>
2020-01-02 17:03:21 +0600

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,9 @@ from open3d import JVisualizer
 from open3d.open3d.geometry import voxel_down_sample,estimate_normals
 import cmath
 from m2dp.M2DP import *
-with open('3d_dataset.pickle', 'rb') as file:
-    pcd_data=pickle.load(file)
-    linestring_data=pickle.load(file)
-    centerpoints_data=pickle.load(file)
-    label=pickle.load(file)
-
-
-a=np.array(pcd_data[0])
-print(M2DP(a))
+#reading demo 3d model
+pcd = o3d.io.read_point_cloud("airplane.ply")
+#converting point cloud to numpy array
+a=np.array(pcd.points)
+#extracting M2DP feature of the point cloud
+desM2DP,A=M2DP(a)
